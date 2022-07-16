e46b1dff71171528b9470204aa4ab5b3bbdfd545
Anoopoo7 <anoopviswanadh007@gmail.com>
2022-07-16 13:40:56 +0530

    password hash upgraded

--- src/main/java/com/fasting/fasting/core/Passwordhandler.java
+++ src/main/java/com/fasting/fasting/core/Passwordhandler.java
@@ -1,12 +1,28 @@
 package com.fasting.fasting.core;
 
+import java.security.MessageDigest;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class Passwordhandler {
 
     public String encript(String password) {
-        return String.valueOf(password.hashCode());
+        // return String.valueOf(password.hashCode());
+
+        try {
+
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.update(password.getBytes());
+            byte[] resultArray = messageDigest.digest();
+            StringBuffer hexString = new StringBuffer();
+            for (int i : resultArray) {
+                hexString.append(Integer.toHexString(0xFF & i));
+            }
+            return hexString.toString();
+        } catch (Exception e) {
+            return password;
+        }
     }
 
 }
