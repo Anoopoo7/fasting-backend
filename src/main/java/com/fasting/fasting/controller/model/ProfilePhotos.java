8c3aa128ee9ea2e9b6f347d56df2a3dd8afd7717
Anoopoo7 <anoopviswanadh007@gmail.com>
2022-07-22 23:47:56 +0530

    photo set to already photoed user

--- src/main/java/com/fasting/fasting/controller/model/ProfilePhotos.java
+++ src/main/java/com/fasting/fasting/controller/model/ProfilePhotos.java
@@ -1,5 +1,6 @@
 package com.fasting.fasting.controller.model;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
@@ -7,6 +8,8 @@ import lombok.Data;
 @Data
 @Document(collection = "fas_profile_photos")
 public class ProfilePhotos {
+    @Id
+    private String id;
     private String userId;
     private boolean active;
     private String photo;
